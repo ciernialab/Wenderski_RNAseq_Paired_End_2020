a66ffc7c9dc743644b6b5ab93b752c3b177d1ac8
Michelle Lu <74755460+luxueyin@users.noreply.github.com>
2021-05-24 13:33:49 -0700

    Update trimmed_unzip.sh

--- trimmed_unzip.sh
+++ trimmed_unzip.sh
@@ -9,7 +9,7 @@
 #######################################################################################
 mkdir -p trimmed_unzip
 
-for sample in `cat SRR_Acc_List.txt`
+for sample in `cat SRR_Acc_List_2.txt`
 do
 
 echo ${sample} "starting unzip"
